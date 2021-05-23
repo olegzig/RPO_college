f554a21466543c358a4e5e078923681ae2f856a7
olegzig <boris37544@gmail.com>
2021-05-23 22:46:04 +0300

    now tables can be open

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -57,7 +57,7 @@ namespace RPO_college
         {
             Menu menu = new Menu();
             menu.Show();
-            this.Close();
+            this.Visibility = Visibility.Hidden;
         }
     }
 }
